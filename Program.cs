821082d1dc92c66472ccae150b1a2a14378d4355
Paul Wallace <paul.wallace@ccalearn.tech>
2018-05-31 16:21:35 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hello, " + name);
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("You are well rested");
+            }
+            else
+            {
+                Console.WriteLine("Your need more sleep");
+            }
+        }
+    }
+}
